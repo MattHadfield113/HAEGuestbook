baf5038212721c54453ff1352e9a3b560e67515b
Matt <matty.hadfield113@gmail.com>
2018-04-10 17:13:11 +0100

    Testing Database

---
+++ includes/classes/database_connect.php
@@ -0,0 +1,50 @@
+<?php
+
+class databaseConnect
+{
+
+    public $db;
+
+
+    public function __construct()
+    {
+
+        $database_host = DatabaseHost;
+        $database_username = DatabaseUsername;
+        $database_password = DatabasePassword;
+        $database_name = DatabaseName;
+
+        $this->db = new PDO("mysql:host=$database_host;dbname=$database_name", $database_username, $database_password);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+    }
+
+    public function Get_All_Posts()
+    {
+
+    }
+
+    public function Get_Single_Post($id)
+    {
+
+    }
+
+    public function Create_Post()
+    {
+
+    }
+
+    public function Edit_Post($id)
+    {
+
+
+    }
+
+    private function __destruct()
+    {
+
+    }
+
+
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 // Load in Config File
-
+include ('/includes/classes/databaseConnecte_connect.php');
 include ('includes/config.inc.php');
 
+new databaseConnect();
