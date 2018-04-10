93c0247052ecd84b985fd16dcb865ff098e5cf70
Matt <matty.hadfield113@gmail.com>
2018-04-10 16:07:50 +0100

    Initial Commit

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -7,3 +7,14 @@
  *
  */
 
+//Database Host
+define('DatabaseHost','hm86340-001.dbaas.ovh.net:35237');
+
+//Database Username
+define('DatabaseUsername','HAETest');
+
+//Database Password
+define('DatabasePassword','HAETest123');
+
+//Database Name
+define('DatabaseName','HAETest');
